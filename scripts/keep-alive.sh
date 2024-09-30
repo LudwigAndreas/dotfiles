dd65f85590ed66e1a4f9c2ae835c35e7b031b497
Ludwig Andreas <landreas-pc@ludwig-mac-pro.local>
2024-09-30 23:37:21 +0300

    added some scripts deleted trash

---
+++ scripts/keep-alive.sh
@@ -0,0 +1,15 @@
+
+#!/bin/bash
+
+# Time interval between keep-alive signals (in seconds)
+INTERVAL=300  # 5 minutes
+
+# Infinite loop to prevent session timeout
+while true
+do
+    # Send a harmless keep-alive signal (you can also use another command if needed)
+    echo "Preventing session timeout..."
+    
+    # Wait for the specified interval
+    sleep $INTERVAL
+done

a9e29881027d65a2f506f5833b3a2b49a640eca7
Ludwig Andreas <landreas-pc@ludwig-mac-pro.local>
2025-03-05 00:42:04 +0300

    added hdfs-uploader

--- tmux/.config/tmux/start-session.sh
+++ tmux/.config/tmux/start-session.sh
@@ -3,13 +3,13 @@
 SESSION_NAME="ghostty"
 
 # Check if the session already exists
-tmux has-session -t $SESSION_NAME 2>/dev/null
+/usr/local/bin/tmux has-session -t $SESSION_NAME 2>/dev/null
 
 if [ $? -eq 0 ]; then
   # If the session exists, reattach to it
-  tmux attach-session -t $SESSION_NAME
+  /usr/local/bin/tmux attach-session -t $SESSION_NAME
 else
   # If the session doesn't exist, start a new one
-  tmux new-session -s $SESSION_NAME -d
-  tmux attach-session -t $SESSION_NAME
+  /usr/local/bin/tmux new-session -s $SESSION_NAME -d
+  /usr/local/bin/tmux attach-session -t $SESSION_NAME
 fi

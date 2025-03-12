4d3c8026dd6fa3ee52084d6c827c62ce37fad414
Ludwig Andreas <landreas-pc@ludwig-mac-pro.local>
2025-03-13 00:51:06 +0300

    added structure into scripts folder, added stdlib for bash funcs in workenv

---
+++ workenv/.config/workenv/shell/stdlib/init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source $WORKENV_CONFIG_PATH/shell/stdlib/log4bash.sh
+source $WORKENV_CONFIG_PATH/shell/stdlib/ask_yn.sh
+source $WORKENV_CONFIG_PATH/shell/stdlib/choose_num.sh
+source $WORKENV_CONFIG_PATH/shell/stdlib/choose_vim.sh
--- workenv/.config/workenv/shell/stdlib/log4bash.sh
+++ workenv/.config/workenv/shell/stdlib/log4bash.sh
@@ -99,26 +99,6 @@ logl() {
     esac
 }
 
-# Template asking [y]es or [n]o (with case insensetive input)
-function ask_yes_no {
-    local message=$1
-    [ -n "$message" ] && log $message && sleep 1
-    printf "%s[*] DO YOU WANT TO PROCEED [Y/N] : " "${cw}"
-    while true; do
-        read -r install
-        # Optional reassignment (only if needed case insensetive parsing)
-        install=$(echo "$install" | tr 'a-z' 'A-Z')
-        case $install in
-            Y|YES)
-                return 0
-            ;;
-            N|NO)
-                return 1
-            ;;
-            *)
-                printf "%s[-] Invalid input. Please answer with 'Y[es]' or 'N[o]' [Y/N] : " "${cw}"
-                continue
-            ;;
-        esac
-    done
-}
+export -f log
+export -f logl
+
